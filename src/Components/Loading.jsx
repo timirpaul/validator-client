44c8f48a1576c411a85162760eb9d2b05b42cc61
Timir <timirpaul57@gmail.com>
2024-03-08 11:55:12 +0530

    login update

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -3,12 +3,10 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="popup loading d-flex align-items-center justify-content-center">
-      
       <h3 className="text-dark"> Validating...</h3>
       <div className="spinner-border text-dark" role="status">
-  <span className="visually-hidden"></span>
-</div>
-
+        <span className="visually-hidden"></span>
+      </div>
     </div>
   );
 };
